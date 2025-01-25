af6a15a12565e2a45a53ce9efa30d9cf74e18e99
ZhouTing <65907235+sleepy-yu@users.noreply.github.com>
2025-01-25 18:34:51 +0800

    Delete Scripts/results directory

--- Scripts/results/generated_code_results/RN_gpt/Entertainment/ShortVideoApp/components_HomeScreen.js
+++
@@ -1,7 +0,0 @@
-13 |
-  14 |       <Image
-> 15 |         source={require('../assets/background-image.png')}
-     |                          ^
-  16 |         style={styles.backgroundImage}
-  17 |         resizeMode='cover'
-  18 |       />
